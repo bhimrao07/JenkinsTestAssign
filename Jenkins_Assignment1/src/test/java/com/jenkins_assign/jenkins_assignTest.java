9c1dc36457716bf2cf037a70eeb8bcc4f8ce6a48
Bhimrao <bhimraokulkarni123@gmail.com>
2025-02-06 20:21:26 +0530

    Committing Jenkins Assign

---
+++ Jenkins_Assignment1/src/test/java/com/jenkins_assign/jenkins_assignTest.java
@@ -0,0 +1,13 @@
+package com.jenkins_assign;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkins_assignTest {
+	@Test
+	public void test()
+	{
+		Reporter.log("Jenkin Test to display",true);
+	}
+
+}
