fd23d83b92feeea65e47e2dfe4f89c5b42b6239a
Bhimrao <bhimraokulkarni123@gmail.com>
2025-02-06 20:41:30 +0530

    Guruji commit

---
+++ Jenkins_Assignment1/src/test/java/com/jenkins_assign/GurujiTest.java
@@ -0,0 +1,15 @@
+package com.jenkins_assign;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Report;
+
+public class GurujiTest {
+
+	@Test
+	public void guruji()
+	{
+		Reporter.log("Guruji is the supermost power",true);
+	}
+}
