08b3051d9804bc75762e443da3fce48f61bf5868
Bhimrao <bhimraokulkarni123@gmail.com>
2025-02-06 20:30:03 +0530

    Message to commit

---
+++ Jenkins_Assignment1/src/test/java/com/jenkins_assign/KillerTest.java
@@ -0,0 +1,14 @@
+package com.jenkins_assign;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class KillerTest {
+	@Test
+	public void killer()
+	{
+		Reporter.log("Killer can come at any time",true);
+	}
+	
+
+}
