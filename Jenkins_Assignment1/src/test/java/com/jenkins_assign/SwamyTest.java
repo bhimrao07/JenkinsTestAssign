0b59a41e3538c9742a5c773180369bf3e8084337
Bhimrao <bhimraokulkarni123@gmail.com>
2025-02-07 12:08:00 +0530

    Hi committing

---
+++ Jenkins_Assignment1/src/test/java/com/jenkins_assign/SwamyTest.java
@@ -0,0 +1,13 @@
+package com.jenkins_assign;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SwamyTest {
+	@Test
+	public void raate()
+	{
+		Reporter.log("I am comin from raate, are you ok?",true);
+	}
+
+}
